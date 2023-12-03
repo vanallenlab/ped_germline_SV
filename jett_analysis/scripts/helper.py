8b56c74bb6df6c6c829b4b252a2dd5d28f96d35c
Jett Crowdis <jettcrowdis@Jetts-MacBook-Pro.local>
2023-12-03 14:46:57 -0500

    quick update before condensing analyses

--- jett_analysis/scripts/helper.py
+++ jett_analysis/scripts/helper.py
@@ -1,4 +1,5 @@
-"""Helper functions for jett's analyses"""
+import requests
+
 def reformat_gs_to_https(gs_path, project_id):
     if gs_path.startswith('gs'):
         gs_path = gs_path[5:]
@@ -17,3 +18,32 @@ def determine_parabola(x, y):
     plot_x = np.linspace(x[0], x[2], 100)
     plot_y = a*plot_x**2 + b*plot_x + c
     return (plot_x, plot_y)
+
+
+def fetch_track_overlap(track, coord):
+    '''Given a UCSC track name, return a binary for if an element in that track overlaps that region.
+       
+       Params:
+       -------
+       track: str, name of UCSC track
+       coord: tuple, (chrom, start, end)
+       
+       Returns:
+       --------
+       overlap: bool, True if track has an element that overlaps that coordinate
+       '''
+    
+    if coord[0][:3] != "chr":
+        raise ValueError('chrom must be prefixed with "chr"')
+    
+    chrom, start, end = coord
+    
+    url = f'https://api.genome.ucsc.edu/getData/track?genome=hg38;track={track};chrom={chrom};start={start};end={end}'
+    resp = requests.get(url).json()
+    
+    if len(resp[track]):
+        return 1
+    else:
+        return 0
+        
+    
